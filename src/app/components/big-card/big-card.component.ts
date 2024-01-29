c1bf91635e989cb5c949ca853ac7611fbf02d412
lucas-pettinelli <pettinellilucas@gmail.com>
2024-01-29 00:09:21 -0300

    Components criados com estilos

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
