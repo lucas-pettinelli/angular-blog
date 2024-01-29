f4b62fdee49af71725db749004788d92c37c04b9
lucas-pettinelli <pettinellilucas@gmail.com>
2024-01-29 02:16:29 -0300

    feat propriedades dinâmicas dos componentes

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  imageCover: string = ""
+  @Input()
+  cardTitle: string = ""
+
+  constructor(){}
+  ngOnInit(): void { }
 
 }
